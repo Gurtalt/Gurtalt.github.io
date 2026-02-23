c0128caef67fb8cad411e68bb1066beeddf5846e
Jay Reid <Jayreid557@outlook.com>
2026-02-23 17:59:19 +0000

    V0.6.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'editor-cache-v0.6.0';
+const CACHE_NAME = 'editor-cache-v0.6.1';
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
@@ -11,15 +11,40 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => 
             cache.addAll(FILES_TO_CACHE))
     );
 });
 
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key); // remove old caches
+                    }
+                })
+            )
+        )
+    );
+
+    self.clients.claim(); // take control immediately
+});
+
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
+        fetch(event.request)
+            .then(response => {
+                const clone = response.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, clone);
+                });
+                return response;
+            })
+            .catch(() => caches.match(event.request))
     );
 });
 
